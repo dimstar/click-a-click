15f05887da24afff5bb3f7a74e4c9bb7161c8f32
dimstar <kylermagee@gmail.com>
2018-06-22 21:04:16 -0700

    listitem created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Listitem from './components/Listitem';
 
 class App extends Component {
   render() {
@@ -10,9 +11,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <div className="row">
+            <Listitem />
+          </div>
+        </div>
+        <footer>
+          
+        </footer>
       </div>
     );
   }
